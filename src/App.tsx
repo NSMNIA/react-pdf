6b6ff5bd9ceca118e9b6367fc4219265697517d0
Kevin Stoop <433243@student.fontys.nl>
2022-03-11 10:53:16 +0100

    pdf is showing with div content
    Signed-off-by: Kevin Stoop <433243@student.fontys.nl>

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,40 @@
 import { useEffect, useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import jsPDF from 'jspdf';
 
 function App() {
-  const [href, setHref] = useState<string>('');
-  const blob = new Blob(['hello world'], { type: 'text/plain'});
+  const [href, setHref] = useState<any>();
+  const pdf = new jsPDF('p', 'pt', 'a4');;
+  pdf.setFontSize(18);
+
+  let blobUrl: any = '';
+  let margins = {
+    top: 10,
+    left: 30,
+    width: 550
+  };
 
   useEffect(()=> {
-    setHref(URL.createObjectURL(blob));
-  }, [])
+    const doc = document.getElementById('test');
+    pdf.html(doc!.innerHTML, {
+      margin: [margins.top, margins.left],
+      width: margins.width,
+      callback: function (pdf) {
+        blobUrl = pdf.output('bloburl')
+        setHref(blobUrl);
+      }
+    });
+  }, [blobUrl])
 
   return (
     <div className="App">
-      { href === '' ? <div>Laden...</div> : <a href={href} target="_blank">Download</a>}
+        <div id="test" style={{display: 'none'}}>
+          <h1 style={{width: '100%', display: 'block', color: 'green'}}>Dit is een test</h1>
+          <p>Hier alle informatie voor de pdf</p>
+        </div>
+
+      { href === '' ? <div>Laden...</div> : <iframe src={href} height={window.innerHeight - 300} width={window.innerWidth - 300}></iframe>}
     </div>
   )
 }
