df656362e340937d941249f54a11da45c9f5d85f
Kevin Stoop <433243@student.fontys.nl>
2022-03-11 13:29:39 +0100

    pdf quotation finished
    Signed-off-by: Kevin Stoop <433243@student.fontys.nl>

--- src/App.tsx
+++ src/App.tsx
@@ -2,39 +2,41 @@ import { useEffect, useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import jsPDF from 'jspdf';
+import Template from './Template';
 
 function App() {
   const [href, setHref] = useState<any>();
-  const pdf = new jsPDF('p', 'pt', 'a4');;
-  pdf.setFontSize(18);
+  let pdfPX = {
+    width: 595,
+    height: 842
+  }
+
+  const pdf = new jsPDF('p', 'pt', [pdfPX.width, pdfPX.height]);
 
   let blobUrl: any = '';
   let margins = {
     top: 10,
-    left: 30,
-    width: 550
+    left: 30
   };
 
   useEffect(()=> {
-    const doc = document.getElementById('test');
-    pdf.html(doc!.innerHTML, {
-      margin: [margins.top, margins.left],
-      width: margins.width,
-      callback: function (pdf) {
-        blobUrl = pdf.output('bloburl')
-        setHref(blobUrl);
-      }
-    });
+      const doc = document.getElementById('template');
+      pdf.html(doc!.innerHTML, {
+        x: 0,
+        y: 0,
+        width: pdfPX.width,
+        callback: function (pdf) {
+          blobUrl = pdf.output('bloburl')
+          setHref(blobUrl);
+        }
+      });
   }, [blobUrl])
 
   return (
     <div className="App">
-        <div id="test" style={{display: 'none'}}>
-          <h1 style={{width: '100%', display: 'block', color: 'green'}}>Dit is een test</h1>
-          <p>Hier alle informatie voor de pdf</p>
-        </div>
+      <Template height={pdfPX.height} width={pdfPX.width}/>
 
-      { href === '' ? <div>Laden...</div> : <iframe src={href} height={window.innerHeight - 300} width={window.innerWidth - 300}></iframe>}
+      { href === '' ? <div>Laden...</div> : <iframe src={href} height={window.innerHeight} width={window.innerWidth - 300}></iframe>}
     </div>
   )
 }
